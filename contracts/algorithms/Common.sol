f55bfaf1439e227430314d26374a6e2d226773cc
Linski <molinski@gideonsp.com>
2021-10-27 09:20:54 -0700

    Moved methods from CommonGuess to Bytecode

--- contracts/algorithms/Common.sol
+++ contracts/algorithms/Common.sol
@@ -7,27 +7,9 @@ import { Bytecode } from "../utils/Bytecode.sol";
     @title Common
 */
 contract CommonGuess{
-  function bytes32ToString(bytes32 x) public returns (string memory) {
-    bytes memory bytesString = new bytes(32);
-    uint charCount = 0;
-    for (uint j = 0; j < 32; j++) {
-        bytes1 char = bytes1(bytes32(uint(x) * 2 ** (8 * j)));
-        if (char != 0) {
-            bytesString[charCount] = char;
-            charCount++;
-        }
-    }
-    bytes memory bytesStringTrimmed = new bytes(charCount);
-    for (uint j = 0; j < charCount; j++) {
-        bytesStringTrimmed[j] = bytesString[j];
-    }
-    return string(bytesStringTrimmed);
-}
-  function readPasswordList(address _addr) public returns (string memory) {
-    bytes32 bc = Bytecode.codeAt(_addr, 0, 32);
-    string memory out = bytes32ToString(bc);
-      
-    return out;
+  function readPassword(address _addr, uint pos) public returns (string memory) {
+    bytes32 bc = Bytecode.codeAt(_addr, pos*32, pos*32 + 31);
+    return Bytecode.bytes32ToString(bc);
   }
   
 }
@@ -52,7 +34,6 @@ contract Common {
 
     // Deploy contract using create
     assembly {
-
          _passwordList := create(0, add(code, 32), mload(code))
      }
 
--- contracts/utils/Bytecode.sol
+++ contracts/utils/Bytecode.sol
@@ -74,4 +74,20 @@ library Bytecode {
       }
     }
   }
+  function bytes32ToString(bytes32 x) public returns (string memory) {
+    bytes memory bytesString = new bytes(32);
+    uint charCount = 0;
+    for (uint j = 0; j < 32; j++) {
+        bytes1 char = bytes1(bytes32(uint(x) * 2 ** (8 * j)));
+        if (char != 0) {
+            bytesString[charCount] = char;
+            charCount++;
+        }
+    }
+    bytes memory bytesStringTrimmed = new bytes(charCount);
+    for (uint j = 0; j < charCount; j++) {
+        bytesStringTrimmed[j] = bytesString[j];
+    }
+    return string(bytesStringTrimmed);
+}
 }
