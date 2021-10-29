00966818ea9bbe4b6efedcca107ba27674c7f9a3
JetDeveloping <jetjadeja@gmail.com>
2021-10-28 21:10:38 -0700

    Fixed return value error

--- contracts/utils/Bytecode.sol
+++ contracts/utils/Bytecode.sol
@@ -59,7 +59,7 @@ library Bytecode {
     uint256 _end
   ) internal view returns (bytes memory oCode) {
     uint256 csize = codeSize(_addr);
-    if (csize == 0) return bytes32("");
+    if (csize == 0) return bytes("");
 
     if (_start > csize) return bytes("");
     if (_end < _start) return bytes("");
