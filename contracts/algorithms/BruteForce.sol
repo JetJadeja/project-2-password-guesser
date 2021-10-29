c7b8749116e57b598159c7d269a899d156e11574
Linski <molinski@gideonsp.com>
2021-10-28 21:47:50 -0700

    Bruteforce guess

--- contracts/algorithms/BruteForce.sol
+++ contracts/algorithms/BruteForce.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: GNU
 pragma solidity 0.8.3;
+
+contract BruteForce{
+    bytes32 pass = 0x0;
+    function Guess() public returns(bytes32){
+        pass = bytes32(uint(pass) + 1);
+        return pass;
+    }
+}
