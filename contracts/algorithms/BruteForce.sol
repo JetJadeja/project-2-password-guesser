7b5a57ec2cc750cfc110157a711f9ad3f895c5b0
JetDeveloping <jetjadeja@gmail.com>
2021-10-30 20:14:53 -0700

    Fixed brute force algorithm

--- contracts/algorithms/BruteForce.sol
+++ contracts/algorithms/BruteForce.sol
@@ -1,34 +1,44 @@
 // SPDX-License-Identifier: GNU
 pragma solidity 0.8.3;
 
-//import { Bytecode } from "../utils/Bytecode.sol";
+import { Bytecode } from "../libraries/utils/Bytecode.sol";
+
 contract BruteForce {
-  function stringToBytes32(string memory source)
-    public
-    pure
-    returns (bytes32 result)
-  {
-    bytes memory tempEmptyStringTest = bytes(source);
-    if (tempEmptyStringTest.length == 0) {
-      return 0x0;
+  /** @notice Use a brute force algorithm to guess the password. */
+  function guess(bytes memory password) external view returns (bool, uint256) {
+    // Allocate memory to store the guessed password.
+    bytes memory currentPassword = new bytes(password.length);
+
+    // Locally store maxRuns to save gas,
+    uint256 counter = 0;
+
+    for (uint256 i = 0; i < password.length; i++) {
+      for (uint8 j = 0; j < 255; j++) {
+        if (bytes1(j) == password[i]) {
+          currentPassword[i] = bytes1(j);
+        }
+
+        counter++;
+      }
     }
 
-    assembly {
-      result := mload(add(source, 32))
-    }
+    return (true, counter);
   }
 
-  bytes32 pass = 0x0;
+  // function Guess(string memory password) public returns (bool, uint256) {
+  //   bytes32 pass;
 
-  function Guess(string calldata cPass) public returns (uint256) {
-    uint256 numRuns = 0;
-    bytes32 cPassE = stringToBytes32(cPass);
-    while (
-      keccak256(abi.encodePacked(pass)) != keccak256(abi.encodePacked(cPassE))
-    ) {
-      pass = bytes32(uint256(pass) + 1);
-      numRuns++;
-    }
-    return numRuns;
-  }
+  //   uint256 numRuns = 0;
+  //   bytes32 encodedPassword = Bytecode.bytesToBytes32(bytes(password));
+
+  //   while (
+  //     keccak256(abi.encodePacked(pass)) !=
+  //     keccak256(abi.encodePacked(encodedPassword))
+  //   ) {
+  //     pass = bytes32(pass + 0x01);
+  //     numRuns++;
+  //   }
+
+  //   return (true, numRuns);
+  // }
 }
