0904881deb2e04753ef225da6fcf585524fb8d19
Linski <molinski@gideonsp.com>
2021-11-02 22:29:33 -0700

    Rewrote BruteForce

--- contracts/algorithms/BruteForce.sol
+++ contracts/algorithms/BruteForce.sol
@@ -7,6 +7,49 @@ contract BruteForce {
   function guess(bytes memory password) external view returns (bool, uint256) {
     // Allocate memory to store the guessed password.
     bytes memory currentPassword = new bytes(password.length);
+
+    // Locally store maxRuns to save gas,
+    uint256 counter = 0;
+    
+    for (uint256 i = 0; i < password.length; i++) {
+      for (uint8 j = 0; j < 255; j++) {
+        if (bytes1(j) == password[i]) {
+          currentPassword[i] = bytes1(j);
+          break;
+        }
+
+        counter++;
+      }
+    }
+
+    return (true, counter);
+  }
+
+}
+
+/*
+  
+*/
+  // function Guess(string memory password) public returns (bool, uint256) {
+  //   bytes32 pass;
+
+  //   uint256 numRuns = 0;
+  //   bytes32 encodedPassword = Bytecode.bytesToBytes32(bytes(password));
+
+  //   while (
+  //     keccak256(abi.encodePacked(pass)) !=
+  //     keccak256(abi.encodePacked(encodedPassword))
+  //   ) {
+  //     pass = bytes32(pass + 0x01);
+  //     numRuns++;
+  //   }
+
+  //   return (true, numRuns);
+  // }
+  /*
+  function guess(bytes memory password) external view returns (bool, uint256) {
+    // Allocate memory to store the guessed password.
+    bytes memory currentPassword = new bytes(password.length);
     
     // Locally store maxRuns to save gas,
     uint256 counter = 0;
@@ -27,44 +70,5 @@ contract BruteForce {
   }
     return (false, counter);
   }
-
-}
-
-/*
-  function guess(bytes memory password) external view returns (bool, uint256) {
-    // Allocate memory to store the guessed password.
-    bytes memory currentPassword = new bytes(password.length);
-
-    // Locally store maxRuns to save gas,
-    uint256 counter = 0;
-
-    for (uint256 i = 0; i < password.length; i++) {
-      for (uint8 j = 0; j < 255; j++) {
-        if (bytes1(j) == password[i]) {
-          currentPassword[i] = bytes1(j);
-        }
-
-        counter++;
-      }
-    }
-
-    return (true, counter);
-  }
-*/
-  // function Guess(string memory password) public returns (bool, uint256) {
-  //   bytes32 pass;
-
-  //   uint256 numRuns = 0;
-  //   bytes32 encodedPassword = Bytecode.bytesToBytes32(bytes(password));
-
-  //   while (
-  //     keccak256(abi.encodePacked(pass)) !=
-  //     keccak256(abi.encodePacked(encodedPassword))
-  //   ) {
-  //     pass = bytes32(pass + 0x01);
-  //     numRuns++;
-  //   }
-
-  //   return (true, numRuns);
-  // }
+  */
 
